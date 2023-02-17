43f47e287fde1b28312b794d58ec81aabccc2d8f
Joseph Anson <josephanson@hotmail.co.uk>
2023-02-17 17:43:39 +0100

    feat: update note page to add ability to delete notes

---
+++ src/components/CreateNote.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { createNewNote } from '~/services/notes'
+
+const noteName = ref('')
+</script>
+
+<template>
+  <Heading h2>
+    Create New Note
+  </Heading>
+  <div class="mb-8 flex">
+    <Input v-model="noteName" class="mr-2" placeholder="Enter name for note" />
+    <Button :disabled="!noteName" class="mr-2 flex-shrink-0" @click="createNewNote(noteName, '', {})">
+      Create note
+    </Button>
+  </div>
+</template>
--- src/pages/notes.vue
+++ src/pages/notes.vue
@@ -1,14 +1,25 @@
 <script lang='ts' setup>
-import { getAllNotes } from '~/services/notes'
+import { deleteNote, getAllNotes, setNotes } from '~/services/notes'
+import { useNotes } from '~/composables/state'
 
 definePageMeta({
   middleware: 'auth',
 })
 
-const { data: notes } = await useAsyncData('notes', async () => {
+const { data: asyncNotes } = await useAsyncData('notes', async () => {
   const { data } = await getAllNotes()
   return data
 })
+
+const notes = useNotes()
+
+if (asyncNotes.value) {
+  setNotes(asyncNotes.value.map(item => ({
+    id: item.id,
+    name: item.name,
+    editor_string: item.editor_string || '',
+  })))
+}
 </script>
 
 <template>
@@ -18,10 +29,20 @@ const { data: notes } = await useAsyncData('notes', async () => {
         Notes
       </Heading>
 
-      <div v-for="note in notes" :key="note.id">
-        <nuxt-link :to="`/note/${note.id}` ">
-          <Button>Open note: {{ note.name }}</Button>
-        </nuxt-link>
+      <CreateNote v-if="notes.length <= 0" />
+
+      <div v-for="note in notes" v-else :key="note.id" class="flex items-center justify-between w-full bg-gray-800 py-1 px-2 rounded mb-1">
+        <p> Name: {{ note.name }}</p>
+
+        <div>
+          <nuxt-link :to="`/note/${note.id}`" class="mr-2">
+            <Button>Open</Button>
+          </nuxt-link>
+
+          <Button class="bg-red-700" @click="deleteNote(note.id)">
+            Delete
+          </Button>
+        </div>
       </div>
     </Container>
   </Page>
