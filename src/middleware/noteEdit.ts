01ea800f8b53db82c71d37727b7e8a31b91a370b
Joseph Anson <josephanson@hotmail.co.uk>
2023-02-21 22:09:57 +0100

    fix: note-edit middleware to get correct route params

--- src/middleware/noteEdit.ts
+++ src/middleware/noteEdit.ts
@@ -3,15 +3,14 @@ function navigateToPreviewOrHome() {
   return navigateTo(route.params?.id ? `/note/${route.params.id}` : '/')
 }
 
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const user = useSupabaseUser()
-  const route = useRoute()
 
   if (!user.value) {
     return navigateToPreviewOrHome()
   }
 
-  const { data: note } = await getNote(getRouterParamsAsString(route.params.id))
+  const { data: note } = await getNote(getRouterParamsAsString(to.params.id))
   const isUsers = isUsersNote(user.value.id, note?.user_id)
 
   if (!isUsers) {
