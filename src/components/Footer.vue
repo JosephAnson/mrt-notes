485651de7257d6c0aa523201fd2643da3a2ac7c7
Joseph Anson <josephanson@hotmail.co.uk>
2023-02-22 11:58:57 +0100

    chore: add github to footer instead of twitter

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,20 @@
 <template>
-  <footer class="mt-10">
+  <footer class="py-10">
     <div class="container">
       <div class="flex items-center justify-between">
-        <p>
-          Created by
-          <a
-            href="https://twitter.com/JosephLeeAnson"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            >Joseph Anson</a
-          >
-        </p>
+        <div>
+          <p>
+            <a
+              href="https://github.com/JosephAnson/mrt-notes/issues"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              class="flex items-center"
+            >
+              <span class="i-carbon-logo-github inline-block mr-2 text-xl" />
+              Submit issues or request features
+            </a>
+          </p>
+        </div>
 
         <div class="flex items-center">
           <p class="mr-2">Visit our guild at:</p>
