6fb25a4e80b8186e19c90787c15f406f2120e656
Joseph Anson <josephanson@hotmail.co.uk>
2023-02-22 11:58:40 +0100

    style: style discord login button

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -20,7 +20,12 @@ const returnUrl = computed(
         <Button @click="signInWithOtp(email, returnUrl)"> Sign In </Button>
       </div>
 
-      <Button v-if="!user" @click="login('discord', returnUrl)">
+      <Button
+        v-if="!user"
+        class="flex items-center !bg-[#5865f2] !hover:bg-[#7983f5]"
+        @click="login('discord', returnUrl)"
+      >
+        <span class="i-carbon-logo-discord inline-block mr-2 text-xl" />
         Login with Discord
       </Button>
       <Button v-else @click="client.auth.signOut()"> Logout </Button>
