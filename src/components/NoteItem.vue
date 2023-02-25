39ec2416f509ad6742785210fc0bb32191fe44f4
Joseph Anson <josephanson@hotmail.co.uk>
2023-02-25 02:46:30 +0100

    fix: only show profile link if username exists

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -17,10 +17,14 @@ const formattedDate = useDateFormat(props.note.created_at, 'YYYY-MM-DD')
         {{ props.note.name }}
       </Heading>
       <div class="flex items-center pb-2">
-        <nuxt-link :to="`/profile/${props.note.username}`">
+        <nuxt-link
+          v-if="props.note.username"
+          :to="`/profile/${props.note.username}`"
+        >
           <span class="i-carbon-user inline-block" /> {{ props.note.username }}
         </nuxt-link>
         <span
+          v-if="props.note.username"
           class="border-r-1 border-solid h-4 border-white pr-2 mr-2 inline-block"
         ></span>
         Created on: {{ formattedDate }}
