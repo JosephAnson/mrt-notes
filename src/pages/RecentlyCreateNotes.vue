fd0cb94250a25889876205a2bff62c6eb5dddaba
Joseph Anson <josephanson@hotmail.co.uk>
2023-02-25 11:23:22 +0100

    fix: show profile info correctly on note

--- src/pages/RecentlyCreateNotes.vue
+++ src/pages/RecentlyCreateNotes.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
+import { createNotes } from '~/services/notes'
+
 const { data: recentlyCreatedNotes } = await useAsyncData(
   'recentlyCreatedNotes',
   async () => await getAllNotes({ limit: 5 })
 )
+
+const notes = recentlyCreatedNotes.value?.map(createNotes) || []
 </script>
 
 <template>
-  <section v-if="recentlyCreatedNotes.length">
+  <section v-if="notes.length">
     <Heading>Recently Created Notes</Heading>
     <div class="mb-8">
-      <NoteItem
-        v-for="note in recentlyCreatedNotes"
-        :key="note.id"
-        :note="note"
-      />
+      <NoteItem v-for="note in notes" :key="note.id" :note="note" />
     </div>
   </section>
 </template>
--- src/pages/RecentlyModifiedNotes.vue
+++ src/pages/RecentlyModifiedNotes.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
+import { createNotes } from '~/services/notes'
+
 const { data: recentlyModifiedNotes } = await useAsyncData(
   'recentlyModifiedNotes',
   async () => await getAllNotes({ order: 'updated_at', limit: 5 })
 )
+
+const notes = recentlyModifiedNotes.value?.map(createNotes) || []
 </script>
 
 <template>
-  <section v-if="recentlyModifiedNotes.length">
+  <section v-if="notes.length">
     <Heading>Recently Modified Notes</Heading>
     <div class="mb-8">
-      <NoteItem
-        v-for="note in recentlyModifiedNotes"
-        :key="note.id"
-        :note="note"
-      />
+      <NoteItem v-for="note in notes" :key="note.id" :note="note" />
     </div>
   </section>
 </template>
