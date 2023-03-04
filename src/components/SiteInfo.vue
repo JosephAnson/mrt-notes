02d6e095718221ddd07794064b26935e5c535791
Joseph Anson <josephanson@hotmail.co.uk>
2023-03-04 01:49:30 +0100

    chore: update site info

--- src/components/SiteInfo.vue
+++ src/components/SiteInfo.vue
@@ -2,7 +2,7 @@
   <Heading h3>Features</Heading>
   <div class="prose mt-8">
     <p>
-      Website dedicated to making raid leaders lives a little easier with ert
+      Website dedicated to making raid leaders lives a little easier with mrt
       note planning, below are some of the features:
     </p>
     <ul>
@@ -15,6 +15,7 @@
       <li>
         Auto transform pasted MRT note, current support is:
         <ul>
+          <li>Spells</li>
           <li>Colors</li>
           <li>Raid Markers</li>
         </ul>
