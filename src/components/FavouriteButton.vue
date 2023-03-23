f5535c7241ac5b0b4c765fc13e50bd0120d82ee7
Joseph Anson <josephanson@hotmail.co.uk>
2023-03-23 23:47:54 +0100

    feat: add tooltip to favourite button

--- src/components/FavouriteButton.vue
+++ src/components/FavouriteButton.vue
@@ -3,14 +3,18 @@ import { useUserStore } from '~/store/user'
 
 const props = defineProps<{ noteId: number }>()
 const userStore = useUserStore()
+
+const hasFavourite = computed(() => userStore.hasFavourite(props.noteId))
 </script>
 
 <template>
-  <button class="flex items-center text-base" @click="userStore.toggleUserFavourite(props.noteId)">
-    <slot />
-    <span
-      class="inline-block ml-1 color-red"
-      :class="userStore.hasFavourite(props.noteId) ? 'i-carbon-favorite-filled' : 'i-carbon-favorite'"
-    />
-  </button>
+  <Tooltip :label="hasFavourite ? 'Remove from favourite notes' : 'Add to favourite notes'">
+    <button class="flex items-center text-base" @click="userStore.toggleUserFavourite(props.noteId)">
+      <slot />
+      <span
+        class="inline-block ml-1 color-red-500"
+        :class="hasFavourite ? 'i-carbon-favorite-filled' : 'i-carbon-favorite'"
+      />
+    </button>
+  </Tooltip>
 </template>
--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,11 +1,76 @@
+<script lang="ts">
+import { onClickOutside } from '@vueuse/core'
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Tooltip',
+  props: {
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'is-primary',
+    },
+    label: {
+      type: String,
+      default: undefined,
+    },
+    contentClass: {
+      type: String,
+      default: undefined,
+    },
+  },
+  setup() {
+    const tooltip = ref(null)
+    const isActive = ref(false)
+
+    function onHover() {
+      open()
+    }
+
+    function onFocus() {
+      open()
+    }
+
+    function open() {
+      isActive.value = true
+    }
+
+    function close() {
+      isActive.value = false
+    }
+
+    /**
+     * Close tooltip if clicked outside.
+     */
+    onClickOutside(tooltip, () => {
+      isActive.value = false
+    })
+
+    return {
+      isActive,
+      tooltip,
+      onHover,
+      onFocus,
+      close,
+    }
+  },
+})
+</script>
+
 <template>
-  <div ref="tooltip" :class="rootClasses">
-    <transition :name="newAnimation">
+  <div ref="tooltip" class="relative">
+    <transition>
       <div
-        v-show="!disabled && active && (tooltipState.isActive || always)"
-        ref="content"
-        :class="['tooltip-content', contentClass]"
-        :style="tooltipState.style"
+        v-show="!disabled && active && isActive"
+        class="absolute top-100% bg-black/50 whitespace-nowrap py-1 px-2 shadow rounded"
+        :class="[contentClass]"
       >
         <template v-if="label">{{ label }}</template>
         <template v-else-if="$slots.content">
@@ -13,173 +78,20 @@
         </template>
       </div>
     </transition>
-    <div
-      ref="trigger"
-      class="tooltip-trigger"
-      @click="onClick"
-      @contextmenu="onContextMenu"
-      @mouseenter="onHover"
-      @focus.capture="onFocus"
-      @mouseleave="close"
-    >
-      <slot ref="slot" />
+    <div class="tooltip-trigger" @mouseenter="onHover" @focus.capture="onFocus" @mouseleave="close">
+      <slot />
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { onClickOutside } from '@vueuse/core';
-import { computed, defineComponent, nextTick, reactive, ref } from 'vue';
-import type { PropType } from 'vue';
-
-interface TooltipState {
-  isActive: boolean;
-  style: {
-    width?: string;
-    heigth?: string;
-    position?: string;
-    zIndex?: string;
-    top?: string;
-    left?: string;
-  };
-  timer: any;
+<style lang="scss" scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.125s ease;
 }
 
-export default defineComponent({
-  name: 'Tooltip',
-  props: {
-    active: {
-      type: Boolean,
-      default: true
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    type: {
-      type: String,
-      default: 'is-primary'
-    },
-    label: {
-      type: String,
-      default: undefined
-    },
-    triggers: {
-      type: Array as PropType<string[]>,
-      default: () => ['hover']
-    },
-    always: Boolean,
-    square: Boolean,
-    dashed: Boolean,
-    multilined: Boolean,
-    size: {
-      type: String,
-      default: 'is-medium'
-    },
-    animated: {
-      type: Boolean,
-      default: true
-    },
-    animation: {
-      type: String,
-      default: 'fade'
-    },
-    contentClass: {
-      type: String,
-      default: undefined
-    },
-    position: {
-      type: String,
-      default: 'is-top'
-    }
-  },
-  setup(props) {
-    const tooltip = ref(null);
-
-    const tooltipState = reactive<TooltipState>({
-      isActive: false,
-      style: {},
-      timer: null
-    });
-
-    const rootClasses = computed<(string | Record<string, boolean | undefined>)[]>(() => {
-      return [
-        'v-tooltip',
-        props.type,
-        props.position,
-        props.size,
-        {
-          'is-square': props.square,
-          'is-always': props.always,
-          'is-multiline': props.multilined,
-          'is-dashed': props.dashed
-        }
-      ];
-    });
-
-    const newAnimation = computed(() => {
-      return props.animated ? props.animation : undefined;
-    });
-
-    function onClick() {
-      if (props.triggers.indexOf('click') < 0) {
-        return;
-      }
-      // if not active, toggle after clickOutside event
-      // this fixes toggling programmatic
-      nextTick(() => {
-        setTimeout(() => open());
-      });
-    }
-
-    function onHover() {
-      if (props.triggers.indexOf('hover') < 0) {
-        return;
-      }
-      open();
-    }
-
-    function onContextMenu(e: Event) {
-      if (props.triggers.indexOf('contextmenu') < 0) {
-        return;
-      }
-      e.preventDefault();
-      open();
-    }
-
-    function onFocus() {
-      if (props.triggers.indexOf('focus') < 0) {
-        return;
-      }
-      open();
-    }
-
-    function open() {
-      tooltipState.isActive = true;
-    }
-
-    function close() {
-      tooltipState.isActive = false;
-    }
-
-    /**
-     * Close tooltip if clicked outside.
-     */
-    onClickOutside(tooltip, () => {
-      tooltipState.isActive = false;
-    });
-
-    return {
-      tooltipState,
-      rootClasses,
-      newAnimation,
-      tooltip,
-      onClick,
-      onContextMenu,
-      onHover,
-      onFocus,
-      close
-    };
-  }
-});
-</script>
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
