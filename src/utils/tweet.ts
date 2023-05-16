518de8515777cb012396dc672cfba187e12b7988
Joseph Anson <josephanson@hotmail.co.uk>
2023-05-16 13:26:02 +0200

    chore: change twitter message

--- src/utils/tweet.ts
+++ src/utils/tweet.ts
@@ -4,8 +4,8 @@ export function tweet(): void {
   const left = (window.screen.width - width) / 2
   const top = (window.screen.height - height) / 2
   const url = `https://twitter.com/share?url=${encodeURIComponent(
-    `${document.location.protocol}//${document.location.host}`
-  )}&text=MRT Notes: Quick build MRT Notes to use in WoW&hashtags=MRTNotes&via=JosephLeeAnson`
+    `https://mrt-notes.vercel.app/`
+  )}&text=MRT Notes: Quick build MRT Notes to use in WoW&hashtags=MRTNotes`
   const opts = `status=1,width=${width},height=${height},top=${top},left=${left}`
 
   window.open(url, '', opts)
