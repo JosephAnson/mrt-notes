bfcd3d46ce73a9e5c91e20ff0fcc241d38c4300b
Joseph Anson <josephanson@hotmail.co.uk>
2023-06-06 12:05:52 +0200

    feat: add updates notification

---
+++ src/components/UpdateNotification.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+const updateNumber = 1
+const show = ref(false)
+
+const update = useLocalStorage('updates', 0)
+
+function close() {
+  show.value = false
+  update.value = updateNumber
+}
+
+onMounted(() => {
+  show.value = update.value !== updateNumber
+})
+</script>
+
+<template>
+  <teleport to=".notices">
+    <transition
+      enter-active-class="transform ease-out duration-300 transition"
+      enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+      enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+      leave-active-class="transition ease-in duration-100"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <div
+        v-if="show"
+        class="pointer-events-auto overflow-hidden rounded-lg bg-green-700 shadow-lg shadow-white/10 ring-1 ring-black ring-opacity-5"
+      >
+        <div class="p-4">
+          <div class="flex items-start">
+            <div class="flex-1">
+              <p class="font-semibold mb-2">Recent Updates</p>
+              <slot></slot>
+            </div>
+            <div class="flex flex-shrink-0">
+              <button
+                type="button"
+                class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                @click="close"
+              >
+                <span class="sr-only">Close</span>
+                <span class="h-5 w-5 i-carbon-close" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </teleport>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -27,6 +27,17 @@ watchOnce(
 
 <template>
   <Section>
+    <UpdateNotification>
+      <div class="prose text-sm">
+        <strong class="!mb-1 block">Fixed: Spell ID imports correctly</strong>
+        <p class="!m-0">
+          To fix your current notes please copy and paste the note back into the editor to correct the spell tags
+          automatically.
+          <code>{spell: 111} converts into {spell:111}</code>
+        </p>
+      </div>
+    </UpdateNotification>
+
     <Container>
       <div class="md:flex justify-between mb-8 md:mb-0">
         <div>
