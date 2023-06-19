003946f44fbea7a5b2e1162560421b2a5e39c8df
Joseph Anson <josephanson@hotmail.co.uk>
2023-06-19 21:56:38 +0200

    fix: homepage navigation was broken due to update notification

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <Navbar />
-    <adsbygoogle />
     <NuxtPage />
     <Footer />
     <div class="notices fixed bottom-10 right-10 z-10" />
+
+    <UpdateNotification>
+      <div class="prose text-sm">
+        <strong class="!mb-1 block">Fixed: Spell ID imports correctly</strong>
+        <p class="!m-0">
+          To fix your current notes please copy and paste the note back into the editor to correct the spell tags
+          automatically.
+          <code>{spell: 111} converts into {spell:111}</code>
+        </p>
+      </div>
+    </UpdateNotification>
   </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -27,17 +27,6 @@ watchOnce(
 
 <template>
   <Section>
-    <UpdateNotification>
-      <div class="prose text-sm">
-        <strong class="!mb-1 block">Fixed: Spell ID imports correctly</strong>
-        <p class="!m-0">
-          To fix your current notes please copy and paste the note back into the editor to correct the spell tags
-          automatically.
-          <code>{spell: 111} converts into {spell:111}</code>
-        </p>
-      </div>
-    </UpdateNotification>
-
     <Container>
       <div class="md:flex justify-between mb-8 md:mb-0">
         <div>
@@ -127,7 +116,6 @@ watchOnce(
       </div>
     </Container>
   </Section>
-  <adsbygoogle />
   <Section class="bg-primary-800">
     <Container>
       <SiteInfo />
