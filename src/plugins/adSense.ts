bdee0105a031401dd69a9c952c08353d9d366ef6
Joseph Anson <josephanson@hotmail.co.uk>
2023-07-13 14:19:16 +0200

    chore: remove adsense plugin as ads are initialised in component

--- src/plugins/adSense.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtPlugin(() => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  (window.adsbygoogle = window.adsbygoogle || []).push({})
-})
