4636906f05916c160ae34f21ae609b7db7666d5c
Joseph Anson <josephanson@hotmail.co.uk>
2023-11-08 11:01:00 +0100

    refactor: remove href options from button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,30 +4,22 @@ const props = withDefaults(
     tag?: string
     disabled?: boolean
     to?: string
-    href?: string
     type?: 'button' | 'submit' | 'reset'
   }>(),
   {
     type: 'button',
   },
 )
-
-const component = computed(() => {
-  if (props.to) return resolveComponent('NuxtLink')
-  return props.href ? 'a' : props.tag || 'button'
-})
 </script>
 
 <template>
-  <component
-    :is="component"
+  <NuxtLink
     :to="props.to"
     :type="props.type"
-    :href="props.href"
     exact-active-class="bg-yellow-500 text-white hover:bg-yellow-600"
     class="bg-primary-100 hover:bg-primary-400 text-white text-center font-bold py-1 px-6 rounded h-8 focus:ring-indigo-500 focus:border-indigo-500 whitespace-nowrap"
     :class="{ 'bg-primary-500 cursor-not-allowed': props.disabled }"
   >
     <slot />
-  </component>
+  </NuxtLink>
 </template>
