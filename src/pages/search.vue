7513f486d9c2ca7fc1fc1c22189495e223e4b95f
Joseph Anson <josephanson@hotmail.co.uk>
2024-01-14 20:21:57 +0100

    refactor: remove notes store

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -1,32 +1,19 @@
 <script setup lang="ts">
-const route = useRoute()
-const router = useRouter()
-const notesStore = useNotesStore()
+import { useRouteQuery } from '@vueuse/router'
+import { getAllNotes, searchAllNotes } from '~/services/notes'
 
-const q = computed(() => route.query?.q ? getRouterParamsAsString(route.query.q) : null)
+const search = useRouteQuery<string>('search', '')
 
-await useAsyncData(`notes-${route.query.q}`, async () => await notesStore.fetchSearchNotes(q.value || ''), {
-  watch: [q],
+const { data: notes } = await useAsyncData(`notes-${search.value}`, async () => {
+  return search.value?.length ? await searchAllNotes(search.value) : await getAllNotes({ limit: 30 })
+}, {
+  watch: [search],
 })
 
 const { data: encounters } = await useFetch('/api/blizzard/latestEncounters')
 
-const search = ref(q.value || '')
-
 function filterEncounter(encounter: string) {
   search.value = encounter
-
-  searchNotes()
-}
-async function searchNotes() {
-  await router.push({
-    name: 'search',
-    query: {
-      q: search.value,
-    },
-  })
-
-  await notesStore.fetchSearchNotes(search.value)
 }
 </script>
 
@@ -38,13 +25,10 @@ async function searchNotes() {
       </Heading>
 
       <div class="bg-gray-900 p-4 rounded mb-8">
-        <form class="flex w-full mb-4 gap-3" @submit.prevent="searchNotes">
+        <form class="flex w-full mb-4 gap-3" @submit.prevent>
           <Field label-for="search" label="Search" class="w-full">
             <Input id="search" v-model="search" type="search" />
           </Field>
-          <Button type="submit">
-            Search
-          </Button>
         </form>
 
         <div v-if="encounters" class="flex flex-wrap">
@@ -54,13 +38,13 @@ async function searchNotes() {
           <div>
             <label class="mb-1 block">Raids</label>
             <div class="flex flex-wrap gap-2 mb-4">
-              <Button v-for="encounter in encounters.raids" :key="encounter" @click="filterEncounter(encounter)">
+              <Button v-for="encounter in encounters.raids" :key="encounter" @click="filterEncounter(encounter.name)">
                 {{ encounter.name }}
               </Button>
             </div>
             <label class="mb-1 block">Mythic+ Dungeons</label>
             <div class="flex flex-wrap gap-2 mb-4">
-              <Button v-for="encounter in encounters.dungeons" :key="encounter" @click="filterEncounter(encounter)">
+              <Button v-for="encounter in encounters.dungeons" :key="encounter" @click="filterEncounter(encounter.name)">
                 {{ encounter.name }}
               </Button>
             </div>
@@ -71,7 +55,7 @@ async function searchNotes() {
       <div id="notelist">
         <Ad ad-slot="2821798678" />
 
-        <NoteItem v-for="note in notesStore.search" :key="note.id" :note="note" />
+        <NoteItem v-for="note in notes" :key="note.id" :note="note" />
       </div>
     </Container>
   </Section>
