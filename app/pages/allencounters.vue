a827ed7b110a2454de81dc5913b08a336181174c
Joseph Anson <josephanson@hotmail.co.uk>
2024-09-06 16:03:25 +0200

    feat: improve all encounters styling

--- app/pages/allencounters.vue
+++ app/pages/allencounters.vue
@@ -42,24 +42,26 @@ const resultsItem = computed(() => results.value.map(i => i.item))
           <div v-for="(expansion, index) in resultsItem" :key="expansion.id" class="mb-12">
             <Ad v-if="index === 1" ad-slot="5501746994" ad-format="fluid" ad-layout-key="-fv+64+31-d5+c4" />
 
-            <div class="mb-8">
-              <Heading h3>
+            <BaseCard class="mb-8">
+              <BaseCardHeader h3>
                 {{ expansion.name }} Raids
-              </Heading>
-              <div v-for="raid in expansion.instances.raids" :key="raid.id" class="mb-4">
-                <Heading h4>
-                  {{ raid.name }}
-                </Heading>
+              </BaseCardHeader>
+              <BaseCardContent>
+                <div v-for="raid in expansion.instances.raids" :key="raid.id" class="mb-4">
+                  <Heading h4>
+                    {{ raid.name }}
+                  </Heading>
 
-                <div class="flex flex-wrap gap-2 mb-2">
-                  <BaseButton v-for="instance in raid.encounters" :key="instance.id" as-child>
-                    <NuxtLink :to="`/search?q=${instance.name}`">
-                      {{ instance.name }}
-                    </NuxtLink>
-                  </BaseButton>
+                  <div class="flex flex-wrap gap-2 mb-2">
+                    <BaseButton v-for="instance in raid.encounters" :key="instance.id" as-child>
+                      <NuxtLink :to="`/search?q=${instance.name}`">
+                        {{ instance.name }}
+                      </NuxtLink>
+                    </BaseButton>
+                  </div>
                 </div>
-              </div>
-            </div>
+              </BaseCardContent>
+            </BaseCard>
           </div>
         </div>
         <div>
@@ -70,24 +72,26 @@ const resultsItem = computed(() => results.value.map(i => i.item))
           <div v-for="(expansion, index) in resultsItem" :key="expansion.id" class="mb-12">
             <Ad v-if="index === 1" ad-slot="7513403484" ad-format="fluid" ad-layout-key="-fv+64+31-d5+c4" />
 
-            <div class="mb-8">
-              <Heading h3>
+            <BaseCard class="mb-8">
+              <BaseCardHeader h3>
                 {{ expansion.name }} Dungeons
-              </Heading>
-              <div v-for="raid in expansion.instances.dungeons" :key="raid.id" class="mb-4">
-                <Heading h4>
-                  {{ raid.name }}
-                </Heading>
+              </BaseCardHeader>
+              <BaseCardContent>
+                <div v-for="raid in expansion.instances.dungeons" :key="raid.id" class="mb-4">
+                  <Heading h4>
+                    {{ raid.name }}
+                  </Heading>
 
-                <div class="flex flex-wrap gap-2 mb-2">
-                  <BaseButton v-for="instance in raid.encounters" :key="instance.id" as-child>
-                    <NuxtLink :to="`/search?q=${instance.name}`">
-                      {{ instance.name }}
-                    </NuxtLink>
-                  </BaseButton>
+                  <div class="flex flex-wrap gap-2 mb-2">
+                    <BaseButton v-for="instance in raid.encounters" :key="instance.id" as-child>
+                      <NuxtLink :to="`/search?q=${instance.name}`">
+                        {{ instance.name }}
+                      </NuxtLink>
+                    </BaseButton>
+                  </div>
                 </div>
-              </div>
-            </div>
+              </BaseCardContent>
+            </BaseCard>
           </div>
         </div>
       </div>
